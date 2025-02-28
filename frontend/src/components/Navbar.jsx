703a7a33083171e3ecbc1f543b9b1e11aa2a0956
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2025-02-27 16:28:12 -0800

    About and Contact pages completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             <img onClick={() => setShowSearch(true)} src={assets.search_icon} alt='search-bar' className='w-5 cursor-pointer'/>
 
             <div className='group relative'>
-                <img src={assets.profile_icon} alt='profile-icon' className='w-5 cursor-pointer'/>
+                <Link to={'/login'}><img src={assets.profile_icon} alt='profile-icon' className='w-5 cursor-pointer'/></Link>
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flex flex-col gap-2 py-3 px-5 bg-slate-100 text-gray-500 rounded w-36'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
