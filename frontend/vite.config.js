e34f1505edd3ab86bf4565f8ed45099d667b43d3
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2025-03-08 19:34:16 -0800

    versel bug removal

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,5 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      'react-router-dom': require.resolve('react-router-dom')
-    }
-  }
+  ]
 })
